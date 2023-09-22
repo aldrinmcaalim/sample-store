73f5c27179a0e13a07f806d39079b40ff8d2cf79
Aldrin Caalim <aldrinmcaalim@gmail.com>
2023-09-22 10:35:59 -0400

    refactored shop component

--- src/routes/shop/Shop.component.jsx
+++ src/routes/shop/Shop.component.jsx
@@ -3,26 +3,20 @@ import { Fragment, useContext } from "react";
 import "./shop.styles.scss";
 
 import { CategoriesContext } from "../../contexts/Categories.context";
-import ProductCard from "../../components/product-card/ProductCard.component";
+import CategoryPreview from "../../components/category-preview/CategoryPreview.component";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <Fragment>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
         return (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => {
-                return <ProductCard key={product.id} product={product} />;
-              })}
-            </div>
-          </Fragment>
+          <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </Fragment>
+    </div>
   );
 };
 
