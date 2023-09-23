df9956af5b19c8da8fada1afff68e0f29ced6f7d
Aldrin Caalim <aldrinmcaalim@gmail.com>
2023-09-22 21:39:10 -0400

    updated paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+import Home from "./routes/home/Home.component";
+import Navigation from "./routes/navigation/Navigation.component";
+import Authentication from "./routes/authentication/Authentication.component";
+import Shop from "./routes/shop/Shop.component";
+import Checkout from "./routes/checkout/Checkout.component";
 
 const App = () => {
   return (
